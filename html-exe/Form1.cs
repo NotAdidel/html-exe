ad6a1263ce28276ee03cc59b99802dfc1b74af55
NotAdidel <NotAdidel@github.com>
2021-02-28 16:21:18 +0100

    Add project files.

---
+++ html-exe/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace html_exe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //nothing
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            //MessageBox.Show(args[0]);
+            //MessageBox.Show(args[1]);
+            webBrowser1.Navigate(args[1]);
+        }
+    }
+}
